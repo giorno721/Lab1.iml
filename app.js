6f5a8beeb2d41271ef0cb466aebfae436f97c7ac
Roman <roman.marushko.pz.2022@lpnu.ua>
2024-03-16 16:26:09 +0200

    PWA added

--- app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register("/sw.js")
-        .then(()=>console.log("Зареєстрований"))
-        .catch(()=>console.log("Отримали помилку"));
-}
