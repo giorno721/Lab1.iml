481b9c1eb169fcd2077ad563f9a3adbe64a0274d
Roman <roman.marushko.pz.2022@lpnu.ua>
2024-04-02 15:39:51 +0300

    laba 2 v.9000

--- public/jsCode.js
+++ public/jsCode.js
@@ -11,236 +11,181 @@ document.querySelector('.burger-menu').addEventListener('click', function() {
     document.querySelector('.sidebar').classList.toggle('open');
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const addStudentBtn = document.getElementById('btnAddStudent');
-    const titleModule = document.getElementById('ModalTitle')
-    const closeBtn = document.getElementById('btnClose');
-    const groupSelect = document.getElementById('groupSelect');
-    const nameInput = document.getElementById('nameInput');
-    const genderSelect = document.getElementById('genderSelect');
-    const birthdateInput = document.getElementById('birthdateInput');
-    const activeRadio = document.getElementById('activeRadio');
-    const inactiveRadio = document.getElementById('inactiveRadio');
-    const tableBody = document.querySelector('.content-table tbody');
-    const studentId = document.getElementById('studentId'); // Збереження id для редагування
-    const formFields = [groupSelect, nameInput, genderSelect, birthdateInput];
-    const deleteConfirmationModal = new bootstrap.Modal(document.getElementById('deleteConfirmationModal'));
-    const confirmDeleteStudentBtn = document.getElementById('confirmDeleteStudent');
-    const modal = new bootstrap.Modal(document.getElementById('AddStudentModalWindow')); // Отримуємо модальне вікно
-    const alertMsg = document.querySelector('.alert-msg');
-    let editingMode = false;
-    let editedRowIndex = null;
-    let rowToDelete = null;
+let freeID = 0;
+let Student = function () {
+    this.id = null;
+    this.group = "";
+    this.name = "";
+    this.gender = "";
+    this.birthday = "";
+    this.status = false;
+}
 
-    const selectedGroupOption = document.getElementById('groupSelect').options[document.getElementById('groupSelect').selectedIndex];
-    const selectedGenderOption = document.getElementById('genderSelect').options[document.getElementById('genderSelect').selectedIndex];
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        let student = new Student();
+        student.id = document.getElementById("studentId").value;
+        student.group = document.getElementById("groupSelect").value;
+        student.name = document.getElementById("nameInput").value;
+        student.gender = document.getElementById("genderSelect").value;
+        student.birthday = document.getElementById("birthdateInput").value;
+        student.status = !!document.getElementById("status").checked;
 
+        let jsonString = JSON.stringify(student);
+        console.log(jsonString);
 
-    // Обробник подій для закриття модального вікна додавання/редагування
-    modal._element.addEventListener('hidden.bs.modal', function (){
-        titleModule.textContent = "Add student";
-        addStudentBtn.textContent = "Add";
-        ClearModalField();
-        alertMsg.style.display = 'none';
-        editingMode = false;
-        editedRowIndex = null;
-        studentId.value = '';
-    })
-    // Додайте обробник подій для кнопки видалення студента
-    document.querySelector('.content-table tbody').addEventListener('click', function(event) {
-        const target = event.target;
-        if (target.classList.contains('bi-x-square')) {
-            const row = target.closest('tr');
-            if (row) {
-                rowToDelete = row;
-                const studentName = row.cells[2].textContent.trim(); // Отримуємо ім'я студента
-                const deleteConfirmationText = document.getElementById('deleteConfirmationText');
-                deleteConfirmationText.textContent = `Are you sure you want to delete the student ${studentName}?`; // Оновлюємо текст у модальному вікні
-                deleteConfirmationModal.show(); // Відображаємо модальне вікно для підтвердження видалення
-            }
+        if(student.id){
+            editStudent(student);
+        }else{
+            student.id=freeID;
+            freeID++;
+            addStudent(student);
         }
-    });
-    // Додайте обробник подій для кнопки підтвердження видалення
-    confirmDeleteStudentBtn.addEventListener('click', function() {
-        if (rowToDelete) {
-            rowToDelete.remove(); // Видаляємо рядок з таблиці
-            deleteConfirmationModal.hide(); // Ховаємо модальне вікно
-            rowToDelete = null; // Скидаємо змінну
-        }
-    });
-
-    // Додайте обробник подій для кнопки Add або Save Changes (залежно від контексту)
-    addStudentBtn.addEventListener('click', function () {
-        // Перевірка, чи форма заповнена
-        if (!groupSelect.value || !nameInput.value || !genderSelect.value || !birthdateInput.value) {
-            alertMsg.style.display = 'initial';
-            return;
-        }
-
-        // Створення об'єкта з даними студента
-        const studentData = {
-            group: groupSelect.options[groupSelect.selectedIndex].text,
-            name: nameInput.value,
-            gender: genderSelect.options[genderSelect.selectedIndex].text,
-            birthdate: formatDate(birthdateInput.value),
-            status: activeRadio.checked ? 'Active' : 'Inactive'
-        };
-        if (studentId.value) {
-            // Редагування студента з вказаним id
-            updateTableRow(studentId, studentData); // Оновлення відповідного рядка таблиці
-        } else {
-            // Додавання нового студента до масиву та таблиці
-            const newId = generateStudentId(); // Генерація унікального id для студента
-            addTableRow(newId, studentData); // Додавання нового рядка в таблицю
-        }
-        //console.log('ID студента:', studentId); // Вивід id у консоль
-        const formData = createFormData();
-        console.log(formData); // Виводимо рядок даних в консоль (для тестування)
-
-        ClearModalField();
+        let modal = bootstrap.Modal.getInstance(document.getElementById("AddStudentModalWindow"));
         modal.hide();
-        editingMode = false;
-        editedRowIndex = null;
-        studentId.value = '';
-    });
 
-    // Функція для оновлення даних рядка в таблиці
-    function updateTableRow(studentId, newData){
-        //const editedRow = tableBody.querySelector(`tr[data-student-id="${studentId + 1}"]`);
-        const editedRow = tableBody.querySelector(`tr:nth-child(${editedRowIndex + 1})`);
-        if (editedRow) {
-            const cells = editedRow.querySelectorAll('td');
-            cells[1].textContent = newData.group;
-            cells[2].textContent = newData.name;
-            cells[3].textContent = newData.gender;
-            cells[4].textContent = newData.birthdate;
-            cells[5].innerHTML = newData.status === 'Active' ? '<i class="bi bi-circle-fill" id="icon-active"></i>' : '<i class="bi bi-circle-fill" id="icon"></i>';
-        } else {
-            console.error('Row not found for updating.');
+    });
+    document.getElementById("confirmDeleteStudent").addEventListener('click',function (){
+        const id = document.getElementById("idOfDelete").value;
+        const row = getRowByDataAttribute('data-id', id);
+        row.parentNode.removeChild(row);
+        let modal = bootstrap.Modal.getInstance(document.getElementById("deleteConfirmationModal"));
+
+        modal.hide();
+    });
+    document.querySelector('.content').addEventListener('click', function (event) {
+        if (event.target.closest("button")?.classList.contains('addOrEdit')){
+            openMainModal(event.target.closest("button"));
         }
-    }
-    function addTableRow(Id, data) {
-        const newRow = document.createElement('tr');
-        newRow.setAttribute('data-student-id', Id); // Встановлення атрибута з унікальним ідентифікатором
-        studentId.value = Id;
-        newRow.innerHTML = `
-            <td><label><input type="checkbox"></label></td>
-            <td>${data.group}</td>
-            <td>${data.name}</td>
-            <td>${data.gender}</td>
-            <td>${data.birthdate}</td>
-            <td>${data.status === 'Active' ? '<i class="bi bi-circle-fill" id="icon-active"></i>' : '<i class="bi bi-circle-fill" id="icon"></i>'}</td>
-            <td><i class="bi bi-pencil-square Icon"></i><i class="bi bi-x-square Icon"></i></td>
-        `;
-        tableBody.appendChild(newRow);
-    }
-
-    // Обробник події для кнопки Close
-    closeBtn.addEventListener('click', function (){
-        modal.hide(); // Закриваємо модальне вікно
-        studentId.value = '';
-        ClearModalField();
-    });
-
-    // Обробник події для кнопки "Close" у хедері модального вікна
-    document.querySelector('#AddStudentModalWindow .close').addEventListener('click', function () {
-        modal.hide(); // Закриваємо модальне вікно
-        studentId.value = '';
-    });
-// Обробник події для кліку на таблиці
-    tableBody.addEventListener('click', function (event) {
-        const target = event.target;
-        // Перевіряємо, чи клікнуто на іконку редагування
-        if (target.classList.contains('bi-pencil-square')) {
-            const row = target.closest('tr'); // Знаходимо батьківський рядок, який містить цю іконку
-            if (!row) {
-                alert("Error: Row not found.");
-                return;
-            }
-            // Очищаємо попередні дані у формі
-            ClearModalField();
-            var studentGroupElement = document.getElementById('groupSelect');
-            const group = row.querySelector('td:nth-child(2)').textContent;
-            const groupOptions = studentGroupElement.options;
-            for (let i = 0; i < groupOptions.length; i++) {
-                if (groupOptions[i].textContent === group) {
-                    groupOptions[i].selected = true;
-                    break;
-                }
-            }
-            var studentGenderElement = document.getElementById('genderSelect');
-            const gender = row.querySelector('td:nth-child(4)').textContent;
-            const genderOptions = studentGenderElement.options;
-            for (let i = 0; i < genderOptions.length; i++) {
-                if (genderOptions[i].textContent === gender) {
-                    genderOptions[i].selected = true;
-                    break;
-                }
-            }
-            // Заповнюємо форму даними обраного рядка для редагування
-            const cells = row.querySelectorAll('td');
-            nameInput.value = cells[2].textContent.trim();
-            // Зчитуємо текстове значення з комірки
-            const birthdateText = cells[4].textContent.trim();
-            // Розбиваємо текстове значення дати за допомогою крапки
-            const parts = birthdateText.split('.');
-            // Перетворюємо дату у формат "YYYY-MM-DD"
-            const formattedDate = `${parts[2]}-${parts[1]}-${parts[0]}`;
-            // Встановлюємо значення у поле введення типу "date"
-            document.getElementById('birthdateInput').value = formattedDate;
-            activeRadio.checked = cells[5].querySelector('i').id === 'icon-active';
-            inactiveRadio.checked = cells[5].querySelector('i').id === 'icon';
-            ChangeInfoUpdate();
-
-            studentId.value = row.getAttribute('data-student-id');
-
-            // Відкриваємо модальне вікно
-            modal.show();
-            editingMode = true; // Увімкнути режим редагування
-            editedRowIndex = Array.from(row.parentNode.children).indexOf(row);
+        if (event.target.closest("button")?.classList.contains('deleteRow')) {
+            openWarningModel(event.target.closest("button"));
         }
     });
-    function createFormData() {
-        const id = document.getElementById('studentId').value;
 
-        const selectedGroupOption = document.getElementById('groupSelect').options[document.getElementById('groupSelect').selectedIndex];
-        const selectedGroupId = selectedGroupOption.value;
 
-        const selectedGenderOption = document.getElementById('genderSelect').options[document.getElementById('genderSelect').selectedIndex];
-        const selectedGenderId = selectedGenderOption.value;
-
-        const formData = {
-            id: parseInt(id),
-            group: parseInt(selectedGroupId),
-            name: nameInput.value,
-            gender: parseInt(selectedGenderId),
-            birthdate:  formatDate(birthdateInput.value),
-            status: activeRadio.checked ? 'True' : 'False'
-        };
-
-        return JSON.stringify(formData); // Повертаємо рядок JSON зі зібраними даними
-    }
-    // Функція для форматування дати у вказаний формат
-    function formatDate(dateString) {
-        const date = new Date(dateString);
-        const day = date.getDate().toString().padStart(2, '0'); // Додаємо нуль спереду, якщо число менше 10
-        const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Місяці в JavaScript починаються з 0
-        const year = date.getFullYear();
-        return `${day}.${month}.${year}`;
-    }
-    function ClearModalField() {
-        formFields.forEach(field => field.value = '');
-    }
-    function  ChangeInfoUpdate()  {
-        addStudentBtn.textContent = "Save Changes";
-        titleModule.textContent = "Edit"
-    }
-    // Функція для генерації унікального id для нового студента
-    function generateStudentId() {
-        const lastRow = tableBody.querySelector('tr:last-child');
-        if (lastRow) {
-            return parseInt(lastRow.getAttribute('data-student-id')) + 1;
-        } else {
-            return 1;
-        }    }
 });
+
+function transformDateFormat(dateString) {
+    let dateObject = new Date(dateString);
+
+    let day = dateObject.getDate();
+    let month = dateObject.getMonth() + 1;
+    let year = dateObject.getFullYear();
+
+    // Format the date in "DD.MM.YYYY" format
+    return `${day < 10 ? '0' + day : day}.${month < 10 ? '0' + month : month}.${year}`;
+}
+function transformDateFormatToISO(dateString) {
+    let parts = dateString.split('.');
+
+    return parts[2] + '-' + parts[1].padStart(2, '0') + '-' + parts[0].padStart(2, '0');
+}
+function updateModal(student) {
+    document.getElementById("studentId").value = student.id ? student.id : "";
+    document.getElementById("groupSelect").value = student.group ;
+    document.getElementById("nameInput").value = student.name;
+    document.getElementById("genderSelect").value = student.gender;
+    document.getElementById("birthdateInput").value = student.birthday ? transformDateFormatToISO(student.birthday) : "";
+    document.getElementById("status").checked = student.status;
+}
+function getRowByDataAttribute(attributeName, attributeValue) {
+    const table = document.getElementById('studentsTable');
+    const rows = table.getElementsByTagName('tr');
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        if (row.getAttribute(attributeName)==attributeValue) {
+            return row;
+        }
+    }
+
+    return null;
+}
+
+function editStudent(student){
+    const row = getRowByDataAttribute('data-id', student.id);
+    const cols = row.querySelectorAll('td');
+
+    cols[1].setAttribute("data-value", student.group);
+    cols[1].textContent = document.querySelector('#groupSelect option[value="' + student.group + '"]').textContent;
+    cols[2].textContent = student.name;
+    cols[3].setAttribute("data-value", student.gender);
+    cols[3].textContent = document.querySelector('#genderSelect option[value="' + student.gender + '"]').textContent;
+    cols[4].textContent = transformDateFormat(student.birthday);
+    if(student.status){
+        cols[5].innerHTML ='<i class="bi bi-circle-fill" id="icon-active"></i>'
+    }else {
+        cols[5].innerHTML ='<i class="bi bi-circle-fill" id="icon"></i>'
+    }
+}
+let openMainModal = function (button) {
+    let student = new Student();
+    let title = "Add student";
+    if (button.getAttribute("data-id") !== "") {
+        title = "Edit student";
+        let tr = button.closest('tr');
+        let columns = tr.querySelectorAll('td');
+        let isActive;
+        columns.forEach(column => {
+            if(column.querySelector('i.status')){
+                isActive =column.querySelector('i.status').classList.contains('active');
+            }
+        });
+        student.id = tr.getAttribute("data-id");
+        student.group = columns[1].getAttribute("data-value");
+        student.name = columns[2].textContent;
+        student.gender = columns[3].getAttribute("data-value");
+        student.birthday = columns[4].textContent;
+        student.status = isActive;
+
+    }
+    updateModal(student);
+    document.getElementById("ModalTitle").innerText = title;
+
+    let modal = new bootstrap.Modal(document.getElementById('AddStudentModalWindow'));
+
+    modal.show();
+
+}
+function openWarningModel(button){
+    let tr = button.closest('tr');
+    let columns = tr.querySelectorAll('td');
+    let name = columns[2].textContent.trim();
+    document.getElementById("idOfDelete").value = button.getAttribute("data-id");
+    document.getElementById("messageForDelete").innerText = "Are you sure you want to delete the student "+name+"?";
+    let modal = new bootstrap.Modal(document.getElementById("deleteConfirmationModal"));
+
+    modal.show();
+}
+function addStudent(student) {
+    let status;
+    if(student.status) {
+        status = '<i class="bi bi-circle-fill" id="icon-active"></i>';
+    }else {
+        status = '<i class="bi bi-circle-fill" id="icon"></i>';
+    }
+    const newRow = document.createElement('tr');
+    newRow.setAttribute("data-id",student.id);
+    newRow.innerHTML =
+        `<td><input type="checkbox" class="table-input"></td>
+                    <td data-value = "${student.group}">${document.querySelector('#groupSelect option[value="' + student.group + '"]').textContent}</td>
+                    <td>${student.name}</td>
+                    <td data-value="${student.gender}">${document.querySelector('#genderSelect option[value="' + student.gender + '"]').textContent}</td>
+                    <td>${transformDateFormat(student.birthday)}</td>
+                    <td>
+                        ${status}
+                    <td>
+                        <div class="d-flex justify-content-center">
+                            <button  class="btn addOrEdit" data-id="${student.id}">
+                                <i class="bi bi-pencil-square edit-btn close-btn table-icons"></i>
+                            </button>
+                            <button  class="btn deleteRow" data-id="${student.id}">
+                               <i class="bi bi-x-square"></i></i>
+                            </button>
+                        </div>
+                    </td>
+          `;
+
+    document.getElementById('studentsTable').getElementsByTagName('tbody')[0].appendChild(
+        newRow);
+}
